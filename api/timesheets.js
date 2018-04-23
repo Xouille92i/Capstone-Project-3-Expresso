cb76d68990e814939bb9ca31413b61a8ad33d89c
Xouille92i <36791285+Xouille92i@users.noreply.github.com>
2018-04-23 16:48:32 +0200

    Add files via upload

---
+++ api/timesheets.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const timesheetsRouter = express.Router({mergeParams: true});
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+//Middleware
+timesheetsRouter.param('timesheetId', (req, res, next, timesheetId) => {
+  const sql = 'SELECT * FROM Timesheet WHERE Timesheet.id = $timesheetId';
+  const values = {$timesheetId: timesheetId};
+  db.get(sql, values, (error, timesheet) => {
+    if (error) {
+      next(error);
+    } else if (timesheet) {
+      next();
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
+// GET /api/employees/:employeeId/timesheets
+timesheetsRouter.get('/', (req, res, next) => {
+  const sql = 'SELECT * FROM Timesheet WHERE Timesheet.employee_id = $employee_id';
+  const values = { $employee_id: req.params.employeeId};
+  db.all(sql, values, (error, timesheets) => {
+    if (error) {
+      next(error);
+    } else {
+      res.status(200).json({timesheets: timesheets});
+    }
+  });
+});
+
+// POST /api/employees/:employeeId/timesheets
+timesheetsRouter.post('/', (req, res, next) => {
+  const timesheetToCreate = req.body.timesheet;
+  const hours = timesheetToCreate.hours,
+        rate = timesheetToCreate.rate,
+        date = timesheetToCreate.date,
+        employeeId = req.params.employeeId;
+  const employeeSql = 'SELECT * FROM Employee WHERE Employee.id = $employeeId';
+  const employeeValues = {$employeeId: employeeId};
+  db.get(employeeSql, employeeValues, (error, employee) => {
+    if (error) {
+      next(error);
+    } else {
+      if (!hours || !rate || !date || !employee) {
+        return res.sendStatus(400);
+      }
+
+      const sql = 'INSERT INTO Timesheet (hours, rate, date, employee_id)' +
+          'VALUES ($hours, $rate, $date, $employeeId)';
+      const values = {
+        $hours: hours,
+        $rate: rate,
+        $date: date,
+        $employeeId: employeeId
+      };
+
+      db.run(sql, values, function(error) {
+        if (error) {
+          next(error);
+        } else {
+          db.get(`SELECT * FROM Timesheet WHERE Timesheet.id = ${this.lastID}`,
+            (error, timesheet) => {
+              res.status(201).json({timesheet: timesheet});
+            });
+        }
+      });
+    }
+  });
+});
+
+// PUT /api/employees/:employeeId/timesheets/:timesheetId
+timesheetsRouter.put('/:timesheetId', (req, res, next) => {
+  const timesheetToModify = req.body.timesheet;
+  const hours = timesheetToModify.hours,
+        rate = timesheetToModify.rate,
+        date = timesheetToModify.date,
+        employeeId = req.params.employeeId;
+  const employeeSql = 'SELECT * FROM Employee WHERE Employee.id = $employeeId';
+  const employeeValues = {$employeeId: employeeId};
+  db.get(employeeSql, employeeValues, (error, employee) => {
+    if (error) {
+      next(error);
+    } else {
+      if (!hours || !rate || !date || !employee) {
+        return res.sendStatus(400);
+      }
+
+      const sql = 'UPDATE Timesheet SET hours = $hours, rate = $rate, ' +
+          'date = $date, employee_id = $employeeId ' +
+          'WHERE Timesheet.id = $timesheetId';
+      const values = {
+        $hours: hours,
+        $rate: rate,
+        $date: date,
+        $employeeId: employeeId,
+        $timesheetId: req.params.timesheetId
+      };
+
+      db.run(sql, values, function(error) {
+        if (error) {
+          next(error);
+        } else {
+          db.get(`SELECT * FROM Timesheet WHERE Timesheet.id = ${req.params.timesheetId}`,
+            (error, timesheet) => {
+              res.status(200).json({timesheet: timesheet});
+            });
+        }
+      });
+    }
+  });
+});
+
+// Delete /api/employees/:employeeId/timesheets/:timesheetId
+timesheetsRouter.delete('/:timesheetId', (req, res, next) => {
+  const sql = 'DELETE FROM Timesheet WHERE Timesheet.id = $timesheetId';
+  const values = {$timesheetId: req.params.timesheetId};
+
+  db.run(sql, values, (error) => {
+    if (error) {
+      next(error);
+    } else {
+      res.sendStatus(204);
+    }
+  });
+});
+
+module.exports = timesheetsRouter;
